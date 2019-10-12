e8690b0e41999a72f057bee5c89c3513c285732d
kakadais <kakadais@gmail.com>
2019-10-12 13:20:20 +0900

    파일 업로드 수정

--- client/examples/ex_upload.js
+++ client/examples/ex_upload.js
@@ -26,6 +26,7 @@ Template.ex_upload.events({
     var db_file = DB_FILES.insert({   //파일 DB에 미리 저장
       file: file
     });
+
     DB_CONTENTS.insert({    //컨텐츠 DB에 저장
       createdAt: new Date(),          //저장 시각
       content: $('#ta-article').val(),//저장 컨텐츠
@@ -34,9 +35,16 @@ Template.ex_upload.events({
         name: db_file.config.file.name  //위 저장 저장 파일명
       }
     });
+
+    Meteor.setTimeout(function() {  // DB에 이미지 삽입 후 link에 바로 접근 시 이미지가 브라우저에 나타나지 않음
+      location.reload();            // 따라서 삽입 후 해당 link 접근에 가능한 시간 경과 후 화면을 갱신 해주는 과정
+    }, 1000);
+  },
+  'click #btn-remove': function() {
+    DB_CONTENTS.remove({_id: this._id});  //선택 컨텐츠를 DB에서 삭제
   },
   'change #inp-file': function(evt, inst) {
-    //inp-file에서 파일을 선택 시 파일명을 라벨에 표시
+    //inp-file에서 파일을 선택 시 파일명을 input 라벨에 표시
     var file = $('#inp-file').prop('files')[0];
     $('#lb-file').text(file.name);
   }
