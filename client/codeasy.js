ffc8c50216fedda5c8cad017ba060c8284b95d38
kakadais <kakadais@gmail.com>
2019-10-21 20:29:08 +0900

    change main.html

---
+++ client/codeasy.js
@@ -0,0 +1,19 @@
+// BlazeLayout.setRoot('body');
+FlowRouter.route('/codeasy',{
+  action: function() {
+    BlazeLayout.render('codeasy');
+  }
+});
+
+
+Template.codeasy.onRendered(function() {
+  $('html').css('height', '100%');
+  $('body').css('height', '100%');
+  $('#__blaze-root').css('height', '100%');
+});
+
+Template.codeasy.onDestroyed(function() {
+  $('html').css('height', '');
+  $('body').css('height', '');
+  $('#__blaze-root').css('height', '');
+});
--- client/main.js
+++ client/main.js
@@ -1,19 +1,5 @@
-// BlazeLayout.setRoot('body');
-FlowRouter.route('/',{
+FlowRouter.route('/', {
   action: function() {
     BlazeLayout.render('main');
   }
-});
-
-
-Template.main.onRendered(function() {
-  $('html').css('height', '100%');
-  $('body').css('height', '100%');
-  $('#__blaze-root').css('height', '100%');
-});
-
-Template.main.onDestroyed(function() {
-  $('html').css('height', '');
-  $('body').css('height', '');
-  $('#__blaze-root').css('height', '');
 });
