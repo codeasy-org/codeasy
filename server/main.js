d09b337ed00fc84c1a736c4664162455d55a98ce
kakadais <kakadais@gmail.com>
2019-11-13 21:12:40 +0900

    기본 사용자(관리자) 등록 예제 추가.

--- server/main.js
+++ server/main.js
@@ -1,3 +1,17 @@
 Meteor.startup(function() {
   // code to run on server at startup
+  if (!Meteor.users.findOne({username: 'admin'})) {
+    //유저(관리자) 생성 예
+    Accounts.createUser({
+      username: 'admin',
+      email: 'admin@admin.admin',
+      password: 'admin!24',
+      profile: {
+        //이름, 주소 등 원하는 사용자 데이터
+        type: 'admin',
+        name: '관리자'
+      }
+    });
+  }
 });
+
