eef72f66812a3fd2f8ed1b7189ad8e10a0bbbf77
kakadais <kakadais@gmail.com>
2019-11-18 02:37:38 +0900

    .

--- client/examples/DATA/ex_posting.js
+++ client/examples/DATA/ex_posting.js
@@ -19,15 +19,24 @@ Template.ex_posting.events({
     if(!title) {
       return alert('제목은 반드시 입력 해 주세요.');
     }
-
-    DB_POSTS.insert({
-      createdAt: new Date(),
-      type: type,
-      name: name,
-      title: title,
-      content: html,
-      readCount: 0
-    });
+    var _id = FlowRouter.getParam('_id');
+    if( _id === 'newPosting') {
+      DB_POSTS.insert({
+        createdAt: new Date(),
+        type: type,
+        name: name,
+        title: title,
+        content: html,
+        readCount: 0
+      })
+    } else {
+      var post = DB_POSTS.findOne({_id: _id});
+      post.type = type;
+      post.name = name;
+      post.title = title;
+      post.content = html;
+      DB_POSTS.update({_id: _id}, post);
+    }
 
     alert('저장하였습니다.');
     $('#inp-type').val('');
