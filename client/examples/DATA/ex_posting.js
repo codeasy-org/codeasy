019c0cb54bc10769aec82dbd73c9a3c00493d2aa
kakadais <kakadais@gmail.com>
2019-11-18 02:13:16 +0900

    .

---
+++ client/examples/DATA/ex_posting.js
@@ -0,0 +1,39 @@
+FlowRouter.template('/ex_posting', 'ex_posting');
+
+
+Template.ex_posting.onRendered(function() {
+  $('#editor').summernote({
+    popover: {},
+    minHeight: 200,
+    maximumImageFileSize: 1048576*10
+  });
+});
+
+Template.ex_posting.events({
+  'click #btn-save': function() {
+    var type = $('#inp-type').val();
+    var name = $('#inp-name').val();
+    var title = $('#inp-title').val();
+    var html = $('#editor').summernote('code');
+
+    if(!title) {
+      return alert('제목은 반드시 입력 해 주세요.');
+    }
+
+    DB_POSTS.insert({
+      createdAt: new Date(),
+      type: type,
+      name: name,
+      title: title,
+      content: html,
+      readCount: 0
+    });
+
+    alert('저장하였습니다.');
+    $('#inp-type').val('');
+    $('#inp-name').val('');
+    $('#inp-title').val('');
+    $('#editor').summernote('reset');
+    window.history.back();
+  },
+})
--- client/examples/DATA/ex_posts.js
+++ client/examples/DATA/ex_posts.js
@@ -1,11 +1,7 @@
 FlowRouter.template('/ex_posts', 'ex_posts');
 
 Template.ex_posts.onRendered(function() {
-  $('#editor').summernote({
-    popover: {},
-    minHeight: 200,
-    maximumImageFileSize: 1048576*10
-  });
+
 });
 
 Template.ex_posts.helpers({
@@ -21,31 +17,6 @@ Template.ex_posts.helpers({
 });
 
 Template.ex_posts.events({
-  'click #btn-save': function() {
-    var type = $('#inp-type').val();
-    var name = $('#inp-name').val();
-    var title = $('#inp-title').val();
-    var html = $('#editor').summernote('code');
-
-    if(!title) {
-      return alert('제목은 반드시 입력 해 주세요.');
-    }
-
-    DB_POSTS.insert({
-      createdAt: new Date(),
-      type: type,
-      name: name,
-      title: title,
-      content: html,
-      readCount: 0
-    });
-
-    alert('저장하였습니다.');
-    $('#inp-type').val('');
-    $('#inp-name').val('');
-    $('#inp-title').val('');
-    $('#editor').summernote('reset');
-  },
   'click #btn-remove': function() {
     if(confirm('삭제 하시겠습니까?')) {
       DB_POSTS.remove({_id: this._id});
