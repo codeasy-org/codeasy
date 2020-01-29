f9d49efe22d972672d5a9b800d92d5543b7c3813
kakadais <kakadais@gmail.com>
2020-01-29 14:03:40 +0900

    로보어드바이저

--- server/main.js
+++ server/main.js
@@ -1,4 +1,25 @@
 Meteor.startup(function() {
+  // {
+  //   _id: 키워드
+  //   major: 전공명
+  // }
+  //
+  // {
+  //   _id: 냅둠
+  //   major: 전공명
+  //   keyword: 키워드
+  // }
+
+  var arr = DB_KEYWORD.findAll()
+  arr.forEach(function(keyword){
+    DB_KEYWORD.update({_id: keyword._id}, {
+      $set: {
+        keyword: keyword._id
+      }
+    })
+  })
+
+
   // code to run on server at startup
   if (!Meteor.users.findOne({username: 'admin'})) {
     //유저(관리자) 생성 예
