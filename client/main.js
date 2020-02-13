374d9c90d569a3607317c20d946d8491c22ca5b6
kakadais <kakadais@gmail.com>
2020-02-13 10:23:17 +0100

    tags

--- client/main.js
+++ client/main.js
@@ -3,10 +3,14 @@ FlowRouter.template('/', 'main');
 Template.main.onRendered(function() {
   // 화면이 그려지고 난 후 제일 먼저 수행
   Session.set('count', 0);
+  Session.set('tags', [])
 });
 
 Template.main.helpers({
   // 화면에 데이터를 전달
+  tags: function() {
+    return DB_TAGS.findAll();
+  },
   count: function() {
     var count = Session.get('count');
     if (!count) {
@@ -19,6 +23,21 @@ Template.main.helpers({
 
 Template.main.events({
   // 화면의 이벤트를 처리
+  'click #btn-save': function() {
+    var tags = Session.get('tags');
+    tags.push($('#inp-tag').val());
+    Session.set('tags', tags);
+    // DB_TAGS.insert({
+    //   createdAt: new Date(),
+    //   name: $('#inp-tag').val()
+    // })
+  },
+  'click #btn-remove': function() {
+    //Session.get('tags')
+    //javascript array에서 특정 텍스트 값을 찾아서
+    // 해당 항목을 지우는 방법
+    // DB_TAGS.remove({_id: this._id});
+  },
   'click #btn-count': function() {
     Session.set('count', Session.get('count')+1);
   }
